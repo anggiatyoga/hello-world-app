01d8a20411dcfb68db4995691a157a8d6467c51b
Anggiat Dwi Prayoga <anggiaty@gmail.com>
2021-08-05 13:28:08 +0700

    second commit

--- app/src/main/java/com/project/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/project/helloworldapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.project.helloworldapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvHello: TextView = findViewById(R.id.tv_hello)
+        tvHello.text = "Hello Anggiat"
     }
 }
